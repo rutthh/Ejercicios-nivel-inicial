39ed1a0db9f4de761239bb10ead400517c9a8f2c
ruth <hofferruth14@gmail.com>
2022-03-17 18:33:56 -0300

    -

---
+++ html/js/e5.js
@@ -0,0 +1,5 @@
+//alert('«Hello Arbi»')
+//document.write('«Hello Arbi»')
+
+var nombre = prompt('Ingrese tu nombre: ')
+alert('Hola ' + nombre + ' ' + 'tu nombre tiene ' + nombre.length + ' letras')
