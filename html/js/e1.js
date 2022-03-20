bf095b552f06dce144a4dbd0b9f95458868ba800
ruth <hofferruth14@gmail.com>
2022-03-20 19:56:11 -0300

    +

--- html/js/e1.js
+++ html/js/e1.js
@@ -1,1 +1,18 @@
-var 
+var nombre = prompt("Ingresa el número que corresponde a tu puesto: 1-Repositor; 2-cajero; 3-supervisor");
+
+if (nombre == 1){
+    var calculo1 = 15890 + (15890*0.10)
+    alert(calculo1);
+} 
+else if (nombre == 2){
+    var calculo2 = 25630.89;
+    alert(calculo2);
+} 
+else if(nombre == 3){
+    var sueldo = 35560.20
+    var calculo3 = sueldo.toFixed(2)
+    var calculo4 = parseInt(calculo3 - (calculo3*0.11))
+    alert('Su sueldo es: $' + calculo3 + ' con un descuento del 11% queda: $'+ calculo4);
+} else {
+    alert("La opcion ingresada no existe")
+}
